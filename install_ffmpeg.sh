2efd466925d6773413f49f47fb1f4591ac93578c
gustavocadev <gustavocadev@gmail.com>
2025-05-24 15:07:27 -0500

    Add installation script for FFmpeg

---
+++ install_ffmpeg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p bin
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ
+mv ffmpeg-*-static/ffmpeg ./bin/ffmpeg
+chmod +x ./bin/ffmpeg
